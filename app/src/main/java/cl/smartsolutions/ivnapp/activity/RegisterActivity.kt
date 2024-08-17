5d655ed3c2d1fccbd6c1c0be90e4380b0162f43a
matfuentesv <mat.fuentes@duocuc.cl>
2024-08-17 16:55:09 -0400

    pantalla register

--- app/src/main/java/cl/smartsolutions/ivnapp/activity/RegisterActivity.kt
+++ app/src/main/java/cl/smartsolutions/ivnapp/activity/RegisterActivity.kt
@@ -1,21 +1,34 @@
 package cl.smartsolutions.ivnapp.activity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import cl.smartsolutions.ivnapp.R
+import cl.smartsolutions.ivnapp.model.User
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var etName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnRegister: Button
+    private val users = mutableListOf<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Vincular las vistas con sus IDs
+        etName = findViewById(R.id.nameEditText)
+        etEmail = findViewById(R.id.emailEditText)
+        etPassword = findViewById(R.id.passwordEditText)
+        btnRegister = findViewById(R.id.registerButton)
+
+
     }
 }
