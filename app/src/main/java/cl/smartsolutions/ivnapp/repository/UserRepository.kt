b96c91c7788b281cd807eaaea6c9b8ff17a1ccd9
matfuentesv <mat.fuentes@duocuc.cl>
2024-08-18 14:32:08 -0400

    se finaliza componente register

--- app/src/main/java/cl/smartsolutions/ivnapp/repository/UserRepository.kt
+++ app/src/main/java/cl/smartsolutions/ivnapp/repository/UserRepository.kt
@@ -20,6 +20,16 @@ object UserRepository {
     fun validateUserByEmail(email: String): User? {
         return users.find { it.getEmail() == email }
     }
+
+    fun getUsers(): MutableList<User> {
+        return mutableListOf(
+            User("Matias", "Fuentes", "matias.fuentes.vasquez@gmail.com", "1234", 29),
+            User("Constanza", "Mundaca", "cmundaca@gmail.com", "admin123", 24),
+            User("Catalina", "Arriagada", "carriagada@gmail.com", "password2", 30),
+            User("Pedro", "Martinez", "pmartinez@gmail.com", "password3", 28)
+        )
+    }
+
     fun createUser(user: User){
         users.add(user)
     }
